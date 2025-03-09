fbb12576df00197b34ceab94c519d64b0e20095b
Felicity Semetey <felicitysemetey628@gmail.com>
2025-03-09 13:59:00 +0000

    new

--- YAI-ProjectA/src/components/Hero.jsx
+++ YAI-ProjectA/src/components/Hero.jsx
@@ -7,12 +7,12 @@ const Hero = () => {
       <div className="hero-content"> 
         <div className="intro-text">
           <h1 style={{ fontFamily: "Inter, serif", fontSize: "30px", fontWeight: "200" }}>ASAASEPA</h1>
-          <h1 style={{ fontSize: "60px" }}>Dual-purpose Organic Fertilizer and Pesticide</h1>
+          <h1 style={{ fontSize: "60px" }}> Organic Fertilizer and Pesticide</h1>
           <p style={{ fontFamily: "Inter, serif", fontSize: "20px", fontWeight: "200" }}>Grow More, Worry Less</p>
           <button style={{display: "in-line-block",  width: "150px", height: "50px", borderRadius: "30px", backgroundColor: "#559409", border: "none", color: "white", fontSize: "16px"}}>Learn more</button>
         </div>
         <div className="image-container">
-          <img src="/fert-nbg2.png" alt="Organic Fertilizer" />
+          <img src="/asaasepa-no_bg11.png" alt="Organic Fertilizer" />
         </div>
       </div>
     </section>
